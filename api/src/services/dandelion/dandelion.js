6102caace999d8c03cdc44c0ae9b718983ee24b0
alexeusgr <alexeusgr@gmail.com>
2022-07-06 11:15:47 +0300

    setting up graphql query

--- api/src/services/dandelion/dandelion.js
+++ api/src/services/dandelion/dandelion.js
@@ -1,5 +1,12 @@
-export const getSome = ({ address }) => {
+import { fetch } from 'cross-undici-fetch'
+
+export const getSome = async ({ address }) => {
+  const response = await fetch(
+    `https://d.graphql-api.testnet.dandelion.link/`
+  )
+  const json = await response.json()
+
   return {
-    address,
+    address: JSON.stringify(json)
   }
 }
--- api/src/services/weather/weather.js
+++ api/src/services/weather/weather.js
@@ -1,9 +1,16 @@
-export const getWeather = ({ zip }) => {
+import { fetch } from 'cross-undici-fetch'
+
+export const getWeather = async ({ zip }) => {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?zip=${zip},US&appid=6dd24d3b23e76a5a3e65871fa0af66f1`
+  )
+  const json = await response.json()
+
   return {
     zip,
-    city: 'City',
-    conditions: 'Hot Lava',
-    temp: 1000,
-    icon: 'https://placekitten.com/100/100',
+    city: json.name,
+    conditions: json.weather[0].main,
+    temp: Math.round(((json.main.temp - 273.15) * 9) / 5 + 32),
+    icon: `http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`
   }
 }
